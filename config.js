c6d355b4136be36e4f6e7572c120d3ad13c2b609
tannyj72-2 <114056000+tannyj72-2@users.noreply.github.com>
2022-09-21 03:44:38 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    titleWeb: "Người yêu của anh ơitha lỗi cho ny đẹp trai của em nhé ",
+    titleWeb: "Người yêu của anh ơi tha lỗi cho ny đẹp trai của em nhé ",
     introTitle: 'bé yêu',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
