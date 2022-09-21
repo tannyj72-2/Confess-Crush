ba52cca5a9c5b7b1eeefe053b8487d05f1683222
tannyj72-2 <114056000+tannyj72-2@users.noreply.github.com>
2022-09-21 03:36:27 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Người yêu của anh ơitha lỗi cho ny đẹp trai của em nhé ",
+    introTitle: 'bé yêu',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -8,13 +8,13 @@ const CONFIG = {
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnYes: 'em tha cho đó kiss em ngán cái đi  <33',
+    btnNo: 'đừng có mơ ai lúc này to mồm thế hả :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh  <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'em biết mà 🥰. Yêu  nhiều nhiều 😘😘',
+    messDesc: 'Sáng mai bên em anh gọi cho em nhé đừng giận anh nữa nhé ko có em đời anh coi như xong anh thương bé yêu nhiều lắm.',
+    btnAccept: 'Okiiiii anh ny dep zai  lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
